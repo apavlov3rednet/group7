907cd4d05b855cb26ffe58c54c126caa4424a14f
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-11-25 10:32:35 +0300

    DOM controller

--- script.js
+++ script.js
@@ -37,10 +37,6 @@
         }
     }
 
-    // DataBase.setValue('name', 'Ivan');
-    // DataBase.setValue('ar', [100, 200, 'Petrov', null, false]);
-    // DataBase.setValue('ob', {test: 'ya', test2: 'google'});
-
     var Owner = {
         user: {},
 
@@ -79,7 +75,7 @@
                     this.user.dateBD = d;
                 }
             }
-            catch() {
+            catch(error) {
                 return false;
             }
         },
@@ -88,4 +84,196 @@
             //обработчик
         }
     }
+
+    var DOM = {
+        /**
+         * @param {*} tagName 
+         * @param {className = '', attrs{}, styles{}, events{}, children[]} params 
+         * @returns 
+         */
+        create: function(tagName = '', params = {}) {
+            if(!tagName)
+                return false;
+
+            let element = document.createElement(tagName);
+
+            if(params.className) {
+                let classList = params.className.split(' ');
+                for (let index of classList) {
+                    element.classList.add(index);
+                }
+            }
+
+            if(typeof params.attrs === 'object') { //attrs - attributes
+                for(let i in params.attrs) {
+                    element.setAttribute(i, params.attrs[i]);
+                }
+            }
+
+            if(typeof params.styles === 'object') {
+                for(let i in params.styles) {
+                    element.style[i] = params.styles[i];
+                }
+            }
+
+            if(typeof params.events === 'object') {
+                for(let i in params.events) {
+                    element.addEventListener(i, params.events[i])
+                }
+            }
+
+            if(typeof params.children === 'object' && params.children instanceof Array) {
+                params.children.forEach(item => element.append(item));
+            }
+
+            return element;
+        },
+
+        adjust: function(element = {}, params = {}) {
+            if(Object.keys(element).length == 0) 
+                return false;
+
+                if(params.className) {
+                    let classList = params.className.split(' ');
+                    for (let index of classList) {
+                        element.classList.add(index);
+                    }
+                }
+    
+                if(typeof params.attrs === 'object') { //attrs - attributes
+                    for(let i in params.attrs) {
+                        element.setAttribute(i, params.attrs[i]);
+                    }
+                }
+    
+                if(typeof params.styles === 'object') {
+                    for(let i in params.styles) {
+                        element.style[i] = params.styles[i];
+                    }
+                }
+    
+                if(typeof params.events === 'object') {
+                    for(let i in params.events) {
+                        element.addEventListener(i, params.events[i])
+                    }
+                }
+    
+                if(typeof params.children === 'object' && params.children instanceof Array) {
+                    params.children.forEach(item => element.append(item));
+                }
+        },
+
+        removeStyle: function(element) {
+            element.removeAttribute('style');
+        },
+
+        clearItem: function(element = {}, all = false) {
+            element.innerHTML = '';
+
+            if(all) {
+                DOM.removeStyle(element);
+                element.removeEventListener();
+
+                // Array.from(element.attributes).forEach(item => {
+                //     element.removeAttribute(item);
+                // })
+            }
+        },
+
+        /**
+         * @param {*} element 
+         * @param {*} total - при true полное удаление, false - очистка
+         */
+        removeItem: function(element = {}, total = false) {
+            if(total) {
+                element.remove();
+            }
+            else {
+                DOM.clearItem(element, true);
+            }
+        }
+    }
+
+    var Model = {
+        obModel: {},
+
+        /**
+         * 
+         * @param {*} parentId 
+         * @param {Array, string, object, nodeList, etc.} params 
+         * @returns 
+         */
+        create: function(parentId, params= {}) {
+            if(!parentId)
+                return false;
+
+            for(let index in params) {
+                let value = params[index];
+
+                if(value instanceof Array || value instanceof Object) {
+                    this.obModel[index] = JSON.stringify(value);
+                }
+                else {
+                    this.obModel[index] = value;
+                }
+            }
+
+            this.obModel.parentId = parentId;
+
+            return this.obModel;
+        }
+    }
+
+    let owners = [
+        {name: 'Ivan', lastName: 'Petrov'},
+        {name: 'Semen', lastName: 'Ivanov'}
+    ],
+    ownersDB = [];
+
+    for(let i in owners) {
+        console.log(owners[i])
+        let name = owners[i].name,
+            lastName = owners[i].lastName;
+
+        ownersDB.push(Owner.setName(name, lastName));
+    }
+
+    console.log(ownersDB);
+    
+    
+
+    // DataBase.setValue('owner', own);
+
+    // let car = Model;
+
+    // car.create(DataBase.getValue('owner'), {
+    //     brand: 'Mersedes',
+    //     model: 'SLK500',
+    //     nomer: 'a000aa123',
+    //     option: ['climat', 'audio', 'podogrev']
+    // });
+
+    // DataBase.setValue('car', car);
+
+    //console.log(car);
+
+    // let test = DOM.create('div', {
+    //     className: 'test someone-class',
+    //     styles: {
+    //         display: 'block',
+    //         'backgroundColor' : '#333',
+    //         width: '100px',
+    //         height: '100px'
+    //     },
+    //     events: {
+    //         click: function() {
+    //             console.log('good');
+    //         }
+    //     },
+    //     children: [
+    //         DOM.create('span'), DOM.create('b')
+    //     ]
+    // });
+
+    // document.body.append(test);
 })(window);
